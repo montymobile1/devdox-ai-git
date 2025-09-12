4b77b71869db816e0bef9c99af92e7031adc3064
Mohamad Ali Jaafar <mohamadali.jaafar@montyholding.com>
2025-09-12 13:20:45 +0300

    feat: Introduced the shared github and gitlab logic from devdox-ai-portal-api

---
+++ src/devdox_ai_git/exceptions/exception_constants.py
@@ -0,0 +1,9 @@
+GIT_PROJECT_FETCH_FAILED = "Unable to retrieve project details"
+
+GIT_PROJECT_LANGUAGE_FETCH_FAILED = "Unable to retrieve Project Language details"
+
+GIT_USER_FETCH_FAILED = "Unable to retrieve user information"
+
+GIT_REPOS_FETCH_FAILED = "Unable to retrieve user repositories"
+
+GIT_AUTH_FAILED = "Provider Authentication failed"
